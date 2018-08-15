d7d19a008d818b65763e436d0d0e6b8b5b2b271d
VeritasSoftware <shantanu_negi@hotmail.com>
2018-08-15 13:42:33 +1000

    added CORS support and Swagger.

--- Webjet.API/Webjet.API/Startup.cs
+++ Webjet.API/Webjet.API/Startup.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace Webjet.API
 {
@@ -20,6 +21,13 @@ namespace Webjet.API
             //Add dependency injection
             services.AddDependencyInjection(this.Configuration);
 
+            services.AddCors();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "Webjet API", Version = "v1" });
+            });
+
             services.AddMvc();
         }
 
@@ -30,6 +38,22 @@ namespace Webjet.API
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            app.UseCors(builder =>
+                            builder.AllowAnyOrigin()
+                                   .AllowAnyMethod()
+                                   .AllowAnyHeader()
+                        );
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
 
             app.UseMvc();
         }
